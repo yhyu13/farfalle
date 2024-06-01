45fe44636d8f3f1f0b09e86d35541838f06b90e3
yhyu13 <yohan680919@163.com>
2024-06-01 15:54:39 +0800

    Update scripts

--- run_docker.sh
+++ run_docker.sh
@@ -2,7 +2,7 @@
 
 docker stop farfalle-frontend-1
 docker rm farfalle-frontend-1
-docker stop farfalle-backend-1
-docker rm farfalle-backend-1
+# docker stop farfalle-backend-1
+# docker rm farfalle-backend-1
 #docker compose -f docker-compose.dev.yaml up
 docker compose -f docker-compose.dev2.yaml up
